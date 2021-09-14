07981058762cd520d983f92a6de79fe17a3959ad
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-15 01:55:55 +0300

    create test for Image model's save method

--- phiimages/tests.py
+++ phiimages/tests.py
@@ -15,3 +15,14 @@ class TestProfileClass(TestCase):
     self.assertTrue(len(profiles) > 0)
 
 
+class TestImageClass(TestCase):
+  def setUp(self):
+    self.new_profile = Profile(name = 'the_phi', profile_photo = 'image.jpg', bio = 'Dedicated coder')
+    self.new_profile.save_profile()
+
+    self.new_image = Image(name = 'Coding Time', captions ='Coding is therapeutic', comments = 'What a cool image and message', photo = 'image.jpeg', likes = '2', profile = 'self.new_profile')
+    self.new_image.save_image()
+
+  def tearDown(self):
+    Profile.objects.all().delete()
+    Image.objects.all().delete()

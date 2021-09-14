61731430063076c4c76e1fffd5486c79d6011ab6
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-15 01:29:53 +0300

    create test for profile model's instance

--- phiimages/tests.py
+++ phiimages/tests.py
@@ -4,4 +4,10 @@ from .models import Image, Profile
 # Create your tests here.
 class TestProfileClass(TestCase):
   def setUp(self):
-      self.new_profile
+      self.new_profile = Profile(name = 'the_phi', profile_photo = 'image.jpg', bio = 'Dedicated coder')
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_profile,Profile))
+
+  def test_save_method(self):
+    

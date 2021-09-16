b47354360ba7791a833fe66d7d1839f79f056d55
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-16 09:38:54 +0300

    create a register_new_user view to enable registration of new users to the application

--- phiimages/views.py
+++ phiimages/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 # Create your views here.
+def register_new_user(request):
+  if request.method == "POST":
+    form = NewUserForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request,user)
+      messages.success(request, 'Registration Successful.')
+      return redirect('index.html')
+    messages.error(request, 'Unsuccessful registration. Invalid information.')
+  form = NewUserForm()
+  return render(request, 'django_registration/registration_form.html', {"registration_form": form} )

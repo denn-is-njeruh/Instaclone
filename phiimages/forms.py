6ca46bda2c4fcee1719a24f2f70369254ff1a498
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-16 08:56:47 +0300

    create a save method for the new user registration form

--- phiimages/forms.py
+++ phiimages/forms.py
@@ -8,3 +8,13 @@ class NewUserForm(UserCreationForm):
   class Meta:
     model = User
     fields = ("username", "email", "password1", "password2")
+
+
+  def save(self,commit=True):
+    user = super(NewUserForm, self).save(commit=False)
+    user.email = self.cleaned_data['email']
+    if commit:
+      user.save()
+    return user
+
+

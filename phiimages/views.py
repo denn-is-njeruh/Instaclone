89ab9f14bf92a67bfcd0a4c30f274f002cba2925
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-16 11:40:22 +0300

    add login function to the application's view to allow functionality of the login template

--- phiimages/views.py
+++ phiimages/views.py
@@ -1,6 +1,8 @@
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 
@@ -20,3 +22,22 @@ def register_new_user(request):
     messages.error(request, 'Unsuccessful registration. Invalid information.')
   form = NewUserForm()
   return render(request, 'registration/registration_form.html', {"registration_form": form} )
+
+
+def login_user(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username,password=password)
+      if user is not None:
+        login(request, user)
+        messages.info(request, f"You are now logged in as {username}.")
+        return redirect('index')
+      else:
+        messages.error(request, f'Invalid Username or password')
+    else:
+      messages.error(request, f'Invalid username or password')
+  form = AuthenticationForm()
+  return render(request, 'registration/login.html', {"login_form": form})
